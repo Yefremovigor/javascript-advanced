3471add479a05363df76ad4a68591912edb0803c
efremov <yefremovigor@yandex.ru>
2024-01-16 17:56:25 +0300

    promis

--- app.js
+++ app.js
@@ -7,31 +7,29 @@
 вывести описание в консоль
 */
 
-const request = new XMLHttpRequest();
-request.open('GET', 'https://pokeapi.co/api/v2/pokemon/ditto');
-request.send();
-request.addEventListener('load', function() {
-    if (this.status !== 200) {
-        return;
-    }
+function getData(url) {
+    return fetch(url)
+        .then(result => {
+            if (!result.ok) {
+                throw new Error(`Request failed with status ${result.status}`);
+            }
+            return result.json()
+        })
+}
 
-    const pokemonData = JSON.parse(this.responseText);
-    const pokemonAbilities = pokemonData.abilities;
-    const firstAbility = pokemonAbilities[0];
-    const abilityURL = firstAbility.ability.url;
-
-    const request = new XMLHttpRequest();
-    request.open('GET', abilityURL);
-    request.send();
-
-    request.addEventListener('load', function() {
-        if (this.status !== 200) {
-            return
-        }
-
-        const abilityData = JSON.parse(this.responseText);
-        const abilityEffects = abilityData.effect_entries;
+getData('https://pokeapi.co/api/v2/pokemon/ditto')
+    .then(data => {
+        const pokemonAbilities = data.abilities;
+        const firstAbility = pokemonAbilities[0];
+        const abilityURL = firstAbility.ability.url;
+        return abilityURL;
+    })
+    .then(url => {
+        return getData(url);
+    })
+    .then(data => {
+        const abilityEffects = data.effect_entries;
         const englishEffect = abilityEffects.find(effect => effect.language.name === 'en').effect;
         console.log(englishEffect);
     })
-});
+    .catch(error => console.log(error));
