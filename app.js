3ca749998e9bb100e89729130965194390afd192
efremov <yefremovigor@yandex.ru>
2024-01-22 13:47:18 +0300

    buttons

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 'use strict';
 
-console.log('Hello!');
+const counter = document.querySelector('.counter');
+const wrapper = document.querySelector('.wrapper');
+
+wrapper.addEventListener('click', (event) => {
+    if (event.target.tagName !== 'BUTTON') {
+        return;
+    }
+
+    for (const element of [...wrapper.children]) {
+        if (element === event.target) {
+            element.classList.add('active');
+            element.innerText = 'Нажата!'
+        } else {
+            element.classList.remove('active');
+            element.innerText = 'Нажми меня';
+        }
+    }
+
+    counter.innerText = Number(counter.innerText) + 1;
+})
