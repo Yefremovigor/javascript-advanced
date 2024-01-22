be551062b7dde45a5a11904e053fc61210852775
efremov <yefremovigor@yandex.ru>
2024-01-22 16:56:19 +0300

    user task

---
+++ task.js
@@ -0,0 +1,9 @@
+'use strict';
+export class Task {
+    constructor(message) {
+        this.message = message;
+    }
+    run() {
+        console.log(this.message);
+    }
+}
---
+++ user.js
@@ -0,0 +1,9 @@
+'use strict';
+export class User {
+    constructor(task) {
+        this.task = task;
+    }
+    do() {
+        this.task.run();
+    }
+}
